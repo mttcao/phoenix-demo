96bce3474796652464a24533d75d2ec89a113514
tya.cao <tya.cao@myob.com>
2017-06-06 16:07:36 +1000

    Create toPayrunResponse function to transform json response

--- web/controllers/payrun_controller.ex
+++ web/controllers/payrun_controller.ex
@@ -5,21 +5,27 @@ defmodule HelloPhoenix.PayrunController do
   use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
-    # payruns = [
-    #
-    #   %{startdate: "20/01/17",
-    #     enddate: "27/01/17",
-    #     date: "2/01/17"},
-    #
-    #   %{startdate: "13/01/17",
-    #     enddate: "20/01/17",
-    #     date: "21/01/17"},
-    #
-    # ]
 
     payruns = Repo.all(Payrun)
+    response = Enum.map(payruns, fn(x) -> toPayrunResponse(x) end)
 
-    json conn, payruns
+    json conn, response
+  end
+
+  def toPayrunResponse(payrun) do
+    payrun_map = %{
+      data: %{
+        type: "payruns",
+        id: payrun.id,
+        attributes: %{
+          startDate: payrun.start_date,
+          endDate: payrun.end_date,
+          payOnDate: payrun.pay_on_date,
+          insertedAt: payrun.inserted_at,
+          updatedAt: payrun.updated_at
+        }
+      }
+    }
   end
 
 end
